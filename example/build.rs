e7864f49663cba333413540658f425ae903f60e9
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-12-12 15:32:32 +0000

    feat: allow changes on svg files to trigger build

--- example/build.rs
+++ example/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("cargo::rerun-if-changed=fonts/example-icons.toml");
+    println!("cargo::rerun-if-changed=fonts/*.svg");
     iced_fontello::build("fonts/example-icons.toml").expect("Generate example-icons font");
 }
