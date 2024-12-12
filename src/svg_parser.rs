23001805e32c6eabe18ed009cb47253257c9f07e
alex-ds13 <145657253+alex-ds13@users.noreply.github.com>
2024-12-12 11:37:53 +0000

    refactor(svg_parser): use imports and apply lint suggestions

--- src/svg_parser.rs
+++ src/svg_parser.rs
@@ -1,11 +1,21 @@
 use std::{collections::BTreeMap, path::Path};
 
+use svg::{
+    node::element::tag::{self, Type},
+    parser::Event,
+};
+use svg_path_ops::pt::PathTransformer;
+
 use crate::{ChosenGlyph, Id, SvgIcon};
 
 pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Option<ChosenGlyph> {
     let mut content = String::new();
-    let parser = svg::open(icon_path, &mut content)
-        .unwrap_or_else(|error| panic!("SVG icon file \"{}\" could not be read: {error}", icon_path.display()));
+    let parser = svg::open(icon_path, &mut content).unwrap_or_else(|error| {
+        panic!(
+            "SVG icon file \"{}\" could not be read: {error}",
+            icon_path.display()
+        )
+    });
     let mut svg_path = String::new();
     let mut group_nest_level = 0;
     let mut svg_view_box = String::new();
@@ -16,11 +26,11 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
     let mut svg_width_percent = 1.0;
     let mut svg_height_percent = 1.0;
     let mut transforms = String::new();
-    let mut transforms_map: BTreeMap<i32, String> = BTreeMap::new();
+    let mut transforms_map = BTreeMap::new();
 
     for event in parser {
         match event {
-            svg::parser::Event::Tag(svg::node::element::tag::Group, svg::node::element::tag::Type::Start, attributes) => {
+            Event::Tag(tag::Group, Type::Start, attributes) => {
                 group_nest_level += 1;
                 if let Some(t) = attributes.get("transform") {
                     let t = t.to_string();
@@ -28,13 +38,13 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
                     transforms_map.insert(group_nest_level, t);
                 }
             }
-            svg::parser::Event::Tag(svg::node::element::tag::Group, svg::node::element::tag::Type::End, _) => {
+            Event::Tag(tag::Group, Type::End, _) => {
                 if let Some(t) = transforms_map.remove(&group_nest_level) {
                     transforms = transforms.trim_end_matches(&t).to_string();
                 }
                 group_nest_level -= 1;
             }
-            svg::parser::Event::Tag(svg::node::element::tag::Path, _, attributes) => {
+            Event::Tag(tag::Path, _, attributes) => {
                 if let Some(p) = attributes.get("d") {
                     let path_transforms = if let Some(t) = attributes.get("transform") {
                         transforms.clone() + " " + t.to_string().as_str()
@@ -43,24 +53,33 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
                     };
                     let mut current_path = p.to_string();
                     if !transforms.is_empty() {
-                        let mut pt = svg_path_ops::pt::PathTransformer::new(current_path);
+                        let mut pt = PathTransformer::new(current_path);
                         pt.transform(path_transforms);
                         current_path = pt.to_string();
                     }
                     svg_path += &current_path;
                 }
             }
-            svg::parser::Event::Tag(svg::node::element::tag::SVG, _, attributes) => {
+            Event::Tag(tag::SVG, _, attributes) => {
                 if let Some(x) = attributes.get("x") {
-                    svg_x = x.to_string().parse().expect("couldn't parse \"x\" from svg icon");
+                    svg_x = x
+                        .to_string()
+                        .parse()
+                        .expect("couldn't parse \"x\" from svg icon");
                 }
                 if let Some(y) = attributes.get("y") {
-                    svg_y = y.to_string().parse().expect("couldn't parse \"y\" from svg icon");
+                    svg_y = y
+                        .to_string()
+                        .parse()
+                        .expect("couldn't parse \"y\" from svg icon");
                 }
                 if let Some(w) = attributes.get("width") {
                     let w = w.to_string();
                     if w.ends_with("%") {
-                        svg_width_percent = w.trim_end_matches("%").parse().expect("couldn't parse \"width\" from svg icon");
+                        svg_width_percent = w
+                            .trim_end_matches("%")
+                            .parse()
+                            .expect("couldn't parse \"width\" from svg icon");
                         svg_width_percent /= 100.0;
                     } else {
                         svg_width = w.parse().expect("couldn't parse \"width\" from svg icon");
@@ -69,7 +88,10 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
                 if let Some(h) = attributes.get("height") {
                     let h = h.to_string();
                     if h.ends_with("%") {
-                        svg_height_percent = h.trim_end_matches("%").parse().expect("couldn't parse \"height\" from svg icon");
+                        svg_height_percent = h
+                            .trim_end_matches("%")
+                            .parse()
+                            .expect("couldn't parse \"height\" from svg icon");
                         svg_height_percent /= 100.0;
                     } else {
                         svg_height = h.parse().expect("couldn't parse \"height\" from svg icon");
@@ -83,15 +105,21 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
         }
     }
 
-    let mut pt = svg_path_ops::pt::PathTransformer::new(svg_path);
+    let mut pt = PathTransformer::new(svg_path);
     let mut actual_height = svg_height;
 
     if !svg_view_box.is_empty() {
         let vb = svg_view_box.split(" ").collect::<Vec<_>>();
         if vb.len() != 4 {
-            panic!("SVG Icon \"{}\" should have viewBox with 4 elements)", icon_path.display());
+            panic!(
+                "SVG Icon \"{}\" should have viewBox with 4 elements)",
+                icon_path.display()
+            );
         }
-        let vb = vb.into_iter().map(|e| e.parse().expect("viewBox element should be a number")).collect::<Vec<f64>>();
+        let vb = vb
+            .into_iter()
+            .map(|e| e.parse().expect("viewBox element should be a number"))
+            .collect::<Vec<f64>>();
         let _x = vb[0];
         let y = vb[1];
         let vb_width = vb[2];
@@ -116,19 +144,17 @@ pub fn parse_image(icon_path: &Path, name: String, code: u64, uid: String) -> Op
     svg_path = pt.to_string();
 
     if !svg_path.is_empty() && svg_width != 0.0 {
-        Some(
-            ChosenGlyph {
-                uid: Id(uid),
-                css: name,
-                code,
-                src: "custom_icons".into(),
-                selected: Some(true),
-                svg: Some(SvgIcon {
-                    path: svg_path,
-                    width: svg_width,
-                }),
-            },
-        )
+        Some(ChosenGlyph {
+            uid: Id(uid),
+            css: name,
+            code,
+            src: "custom_icons".into(),
+            selected: Some(true),
+            svg: Some(SvgIcon {
+                path: svg_path,
+                width: svg_width,
+            }),
+        })
     } else {
         None
     }
